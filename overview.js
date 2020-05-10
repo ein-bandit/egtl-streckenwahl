4f10abbd87f778f792226c7f59e731c10fe14a1c
ein-bandit <ein-bandit@tuta.io>
2020-05-10 21:33:10 +0200

    added slider on overview.

--- overview.js
+++ overview.js
@@ -17,6 +17,61 @@ for (var i = 0; i < track_length; i++) {
     document.getElementById('all-tracks').appendChild(previewWrap);
 }
 
+var pages = Math.ceil(track_length / 8);
+var currentPage = 0;
+
+var slider = new Siema({
+    selector: '#all-tracks',
+    duration: 200,
+    easing: 'ease-out',
+    perPage: 8,
+    startIndex: 0,
+    draggable: false,
+    multipleDrag: false,
+    threshold: 20,
+    loop: false,
+    rtl: false,
+    onInit: () => {},
+    onChange: () => {},
+});
+
+function updateSliderText() {
+    $('.slider-text .text').text(`${currentPage + 1} / ${pages}`);
+}
+
+setTimeout(() => {
+
+    $('#all-tracks').append('<div class="preview-border"></div>');
+    $('#all-tracks-wrapper').append('<div class="slider-text"><div class="text"></div></div>');
+    updateSliderText();
+    setTimeout(() => {
+        $('.slider-text').append('<div class="prev">&lt;</div><div class="next">&gt;</div>');
+
+        $('.slider-text .next').click(function(el) {
+            $('.preview-track-' + prevIndex).trigger('click').trigger('mouseout');
+
+            console.log("clicked next");
+            if (currentPage === pages - 1) {
+                return;
+            }
+            currentPage += 1;
+            updateSliderText();
+            slider.next(8);
+        });
+        $('.slider-text .prev').click(function(el) {
+            $('.preview-track-' + prevIndex).trigger('click').trigger('mouseout');
+
+            if (currentPage === 0) {
+                return;
+            }
+            currentPage -= 1;
+            updateSliderText();
+            slider.prev(8);
+        });
+    }, 0);
+
+}, 0);
+
 var prevIndex = -1;
 var hovering = false;
 
@@ -25,9 +80,10 @@ $('.preview-track').mouseover(function(el) {
     if (prevIndex > -1) {
         return;
     }
+
     hovering = true;
     $('.track-wrap').addClass('show');
-    $('#track-area .track-wrap-highlight').css('background-image', 'url(' + tracks[$(el.target).index() - 1].imgThumb + ')');
+    $('#track-area .track-wrap-highlight').css('background-image', 'url(' + tracks[$(el.target).parent().index()].imgThumb + ')');
 }).mouseout(function(el) {
     if (prevIndex > -1) {
         return;
@@ -43,23 +99,28 @@ $('.preview-track').mouseover(function(el) {
 
 //preview track click: show big image.
 $('.preview-track').click(function(el) {
-
-    if (prevIndex === $(el.target).index() - 1) {
+    var elem = $(el.target).parent();
+    if (prevIndex === elem.index()) {
         $('.preview-border').removeClass('show');
         $('#track-area').removeClass('big');
         prevIndex = -1;
         setTimeout(() => {
-            $('#track-area .track-wrap-highlight').css('background-image', 'url(assets/tracks/track-' + ($(el.target).index() % track_length) + '.png')
+            $('#track-area .track-wrap-highlight').css('background-image', 'url(assets/tracks/thumbs/track-' + (elem.index() + 1) + '-thumb.png')
         }, 250);
     } else {
         $('.preview-border').addClass('show');
 
-        prevIndex = $(el.target).index() - 1;
+        prevIndex = elem.index();
         console.log('move border to ', prevIndex, $(el.target).position());
-        $('.preview-border').css('top', $(el.target).position().top);
-        $('.preview-border').css('left', $(el.target).position().left);
-        $('.preview-border').css('width', $(el.target).width() - 2);
-        $('.preview-border').css('height', $(el.target).height() - 4);
+        var rect = el.target.getBoundingClientRect();
+        console.log(rect);
+        $('.preview-border').css('top', 0);
+        $('.preview-border').css('left', rect.left);
+        //dirty hack to show border on last track completely.
+        console.log(prevIndex);
+        var extra = ((prevIndex % 8) === 7) || (prevIndex === track_length - 1) ? 2 : 0;
+        $('.preview-border').css('width', (elem.width() - 2) - extra);
+        $('.preview-border').css('height', elem.height() - 4);
 
         $('#track-area').addClass('big');
         $('#track-area .track-wrap-highlight').addClass('show');
